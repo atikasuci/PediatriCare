40e1c0c9b41ef9c0c7bb56b9784e719c11c57058
rizkan03 <61906974+rizkan03@users.noreply.github.com>
2020-04-15 22:59:12 +0700

    Update Login.php

--- application/views/Login.php
+++ application/views/Login.php
@@ -1,31 +1,51 @@
-<div style="padding-left: 30%">
-    <h2>Login</h2>
-    <form action="<?= site_url('LoginController') ?>" method="post" accept-charset="utf-8">
-        <table>
-            <tbody>
-                <tr>
-                    <td>Username</td>
-                </tr>
+<html>
+  <head>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/login.css">
+    <!------ Include the above in your HEAD tag ---------->
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 col-xl-9 mx-auto">
+          <div class="card card-signin flex-row my-5">
+            <div class="card-img-left d-none d-md-flex">
+              <img src="assets/images/login/foto_login.png" width="500px">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title text-center">Member Login</h5>
+              <form class="form-signin" >
+                <div class="form-group">
+                  <input type="username" class="form-control" id="inputUsername" placeholder="Username">
+                </div>
 
-                <tr>
-                    <td><input type="username" name="username" class="textlog" style="padding: 5px; padding-right: 225px;"></td>
-                </tr>
+                <div class="form-group" >
+                  <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                </div>
 
-                <tr>
-                    <td style="padding-top: 10px">Password</td>
-                </tr>
+                <button type="submit" class="btn btn-primary" >Login</button>
 
-                <tr>
-                    <td><input type="password" name="password" class="textlog" style="padding: 5px; padding-right: 225px"></td>
-                </tr>
+                <br>
+                  <p>or</p>
+                </br>
 
-                <tr>
-                    <td style="padding-top: 10px"><input type="submit" value="Sign In" id="signin" class="sign"></td>
-                </tr>
-            </tbody>
-        </table>
-    </form><br>
-    <div class="text-left">
-        <a class="btn btn-secondary btn-sm active" href="<?= base_url('RegistController'); ?>">Create an Account!</a>
+                <div class="signup">
+                  <a href="<?= base_url('RegistController'); ?>">Sign Up</a>
+                </div>
+
+                <div class="text-left">
+                  <a class="btn btn-secondary btn-sm active" href="">back</a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </body>
+</html>
+
+  
+   
