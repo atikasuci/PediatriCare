73879afcdebdc8de0f40844990e3ed9ba36dbe59
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2020-04-17 16:37:42 +0700

    Update Login.php

--- application/views/Login.php
+++ application/views/Login.php
@@ -3,7 +3,7 @@
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/login.css">
+    <link rel="stylesheet" href="<?php base_url(); ?>assets/css/login.css" type="text/css" />
     <!------ Include the above in your HEAD tag ---------->
   </head>
   <body>
