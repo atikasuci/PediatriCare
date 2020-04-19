ce36d07d7064a43579f6401ce14c034a843fa7d5
rizkan03 <61906974+rizkan03@users.noreply.github.com>
2020-04-19 11:43:41 +0700

    Update index.php

--- application/views/RumahSakit/index.php
+++ application/views/RumahSakit/index.php
@@ -1,6 +1,51 @@
+<html lang="en">   
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/login.css">  -->   
+</head>
+<body>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <h2 class="display-4">Anda Ingin Konsultasi Seputar Anak ?</h2>
+            <p class="lead">Tanyakan kepada kami secara mudah dan cepat</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">klik disini</a>
+        </div>
+    </div>
 
-<br><br>
-<h1> THIS IS RUMAH SAKIT</h1>
-<p>LAH KETEMU LAGI, <?= $user['username']; ?></p>
+    <div class="container">
+        <div class="row">
+            <div class="media position-relative">
+                <img src="assets/images/rs/icon.png" class="mr-3" alt="..." width="200" height="150">
+                <div class="media-body">
+                    <h5 class="mt-0">RS Muhammadiyah Bandung</h5>
+                    <p>
+                    Jl. K.H. Ahmad Dahlan No 53, Turangga,<br>
+                    Kec. Lengkong, Kota Bandung, Jawa Barat 40264 <br>
+                    (022) 7301062
+                    </p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="media position-relative">
+                <img src="assets/images/rs/icon.png" class="mr-3" alt="..." width="200" height="150">
+                <div class="media-body">
+                    <h5 class="mt-0">RS Hasan Sadikin Bandung</h5>
+                    <p> Jl. Pasteur No. 38 Bandung,<br>
+                    Kel. Pasteur Kec. Sukajadi 40161,<br>
+                    (022)2032216</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
 
 
