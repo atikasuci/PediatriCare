14391d34d3edf65f59c56e0fe4fc48dad1b6cc48
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2020-04-19 21:59:11 +0700

    Update KonsultasiModel.php

--- application/models/KonsultasiModel.php
+++ application/models/KonsultasiModel.php
@@ -11,13 +11,13 @@ class KonsultasiModel extends CI_Model
 
     public function AddKonsultasi($data)
     {
-        $data = [
-            "judul" => $this->input->post('judul', true),
-            "username" => $this->input->post('username', true),
-            "dokter" => $this->input->post('dokter', true),
-            "question" => $this->input->post('question', true),
-            "answer" => $this->input->post('answer', true)
-        ];
+        // $data = [
+        //     "judul" => $this->input->post('judul', true),
+        //     "username" => $this->input->post('username', true),
+        //     "dokter" => $this->input->post('dokter', true),
+        //     "question" => $this->input->post('question', true),
+        //     "answer" => $this->input->post('answer', true)
+        // ];
         return $this->db->insert('konsultasi', $data);
     }
 
@@ -30,21 +30,5 @@ class KonsultasiModel extends CI_Model
     public function DeleteKonsultasi($id_konsultasi)
     {
         return $this->db->delete('konsultasi', ['id_konsultasi' => $id_konsultasi]);
-    }
-    public function view()
-    {
-        return $this->db->get('konsultasi')->result();
-    }
-    public function Save($upload)
-    {
-        $data = array(
-            'judul' => $this->input->post('input_judul'),
-            'username' => $this->input->post('input_username'),
-            'dokter' => $this->input->post('input_dokter'),
-            'question' => $this->input->post('input_question'),
-            'answer' => $this->input->post('input_answer'),
-        );
-
-        $this->db->insert('konsultasi', $data);
     }
 }
