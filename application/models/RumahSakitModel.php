ed2e549474c654946bff182a0ef007db2eaa8168
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2020-04-19 21:59:38 +0700

    Update RumahSakitModel.php

--- application/models/RumahSakitModel.php
+++ application/models/RumahSakitModel.php
@@ -1,11 +1,41 @@
 <?php
 
-class RumahSakitModel extends CI_model
+class RumahSakitModel extends CI_Model
 {
-	public function getAllRumahSakit()
-	{
-		// get all data
-		$query = $this->db->query("SELECT * FROM rumahsakit");
-		return $query->result_array();
-	}
+    public function getAllRumahSakit()
+    {
+        // get all data
+        $query = $this->db->query("SELECT * FROM rumahsakit");
+        return $query->result_array();
+    }
+
+    public function addRumahSakit($data)
+    {
+        //use query builder to insert $data to table "rumahsakit"
+        // $data = [
+        //     "id_rumahsakit" => '',
+        //     "nama" => $this->input->post('nama', true),
+        //     "alamat" => $this->input->post('alamat', true),
+        //     "notelp" => $this->input->post('notelp', true)
+        // ];
+        $this->db->insert('rumahsakit', $data);
+    }
+
+    public function deleteRumahSakit($id)
+    {
+        $this->db->where('id', $id);
+        return $this->db->delete('rumahsakit');
+    }
+
+    public function updateRumahSakit($id)
+    {
+        $data = [
+            "id_rumahsakit" => $id,
+            "nama" => $this->input->post('nama', true),
+            "alamat" => $this->input->post('alamat', true),
+            "notelp" => $this->input->post('notelp', true)
+        ];
+        $this->db->where('id_rumahsakit', $id);
+        $this->db->update('rumahsakit', $data);
+    }
 }
