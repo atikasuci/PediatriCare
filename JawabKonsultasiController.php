46f4662e190a3744d7cbe837257a73111c2c598c
fenrilius <52090483+fenrilius@users.noreply.github.com>
2020-04-19 22:53:01 +0700

    Add files via upload

---
+++ JawabKonsultasiController.php
@@ -0,0 +1,19 @@
+<?php
+
+class JawabKonsultasiController extends CI_Controller
+{
+	public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+    	$data['judul'] = 'About Us';
+        $data['user'] = $this->session->userdata('user');
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('JawabKonsultasi', $data);
+        $this->load->view('templates/footer');
+    }
+}
