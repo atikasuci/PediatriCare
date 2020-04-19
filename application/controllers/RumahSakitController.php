29644f15b1fe1ae658a8b24543a45d0814eae392
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2020-04-19 21:56:29 +0700

    Update RumahSakitController.php

--- application/controllers/RumahSakitController.php
+++ application/controllers/RumahSakitController.php
@@ -2,21 +2,60 @@
 
 class RumahSakitController extends CI_Controller
 {
-	public function __construct()
+    public function __construct()
     {
         parent::__construct();
-        // load RumahSakitModel
-        $this->load->model('RumahsakitModel');
-
+        // load RumahSakitMode
+        $this->load->model('RumahSakitModel');
+        $this->load->library('form_validation');
     }
 
     public function index()
     {
-    	$data['judul'] = 'Rumah Sakit';
+        $data['judul'] = 'Rumah Sakit';
         $data['user'] = $this->session->userdata('user');
 
+        $data['rumahsakit'] = $this->RumahSakitModel->getAllRumahSakit();
         $this->load->view('templates/header', $data);
         $this->load->view('RumahSakit/index', $data);
         $this->load->view('templates/footer');
+    }
+
+    public function addRs()
+    {
+        $this->form_validation->set_rules('nama', 'nama', 'required');
+        $this->form_validation->set_rules('alamat', 'alamat', 'required');
+        $this->form_validation->set_rules('notelp', 'notelp', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $data['judul'] = 'Rumah Sakit';
+            $data['rumahsakit'] = $this->RumahSakitModel->getAllRumahSakit();
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('RumahSakit/index');
+            $this->load->view('templates/footer');
+        } else {
+            $add = [
+                "id_rumahsakit" => '',
+                "nama" => $this->input->post('nama', true),
+                "alamat" => $this->input->post('alamat', true),
+                "notelp" => $this->input->post('notelp', true)
+            ];
+            $this->RumahSakitModel->addRumahSakit();
+            // load rscon
+
+        }
+        redirect('RumahSakitController');
+    }
+
+    public function delete($id)
+    {
+        $this->RumahSakitModel->deleteRumahSakit($id);
+        redirect('RumahSakitController');
+    }
+
+    public function FunctionName($value = '')
+    {
+        # code...
     }
 }
