44147f9a25a439ff5d94aac8a9da9d434a8e2ed0
fenrilius <52090483+fenrilius@users.noreply.github.com>
2020-04-19 19:39:25 +0700

    Update AboutUs.php

--- application/views/AboutUs.php
+++ application/views/AboutUs.php
@@ -1,39 +1,179 @@
-<h2>About Us</h2><br><br>
-<div class="row row-cols-1 row-cols-md-2">
-    <div class="col mb-2">
-        <div class="card">
-            <img src="<?php echo base_url() . 'assets/images/dre.jpg' ?>" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Andrea Rahmadanisya</h5>
-                <p class="card-text">drea(1301184146) as a BackEnd.</p>
-            </div>
-        </div>
+<style>
+.row.heading h2 {
+    color: #fff;
+    font-size: 52.52px;
+    line-height: 95px;
+    font-weight: 400;
+    text-align: center;
+    margin: 0 0 40px;
+    padding-bottom: 20px;
+    text-transform: uppercase;
+}
+ul{
+  margin:0;
+  padding:0;
+  list-style:none;
+}
+.heading.heading-icon {
+    display: block;
+}
+.padding-lg {
+	display: block;
+	padding-top: 60px;
+	padding-bottom: 60px;
+}
+.practice-area.padding-lg {
+    padding-bottom: 55px;
+    padding-top: 55px;
+}
+.practice-area .inner{ 
+     border:1px solid #999999; 
+	 text-align:center; 
+	 margin-bottom:28px; 
+	 padding:40px 25px;
+}
+.our-webcoderskull .cnt-block:hover {
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.3);
+    border: 0;
+}
+.practice-area .inner h3{ 
+    color:#3c3c3c; 
+	font-size:24px; 
+	font-weight:500;
+	font-family: 'Poppins', sans-serif;
+	padding: 10px 0;
+}
+.practice-area .inner p{ 
+    font-size:14px; 
+	line-height:22px; 
+	font-weight:400;
+}
+.practice-area .inner img{
+	display:inline-block;
+}
+
+.our-webcoderskull{
+  background: url(<?php echo base_url(); ?>assets/images/bgAU.jpg) no-repeat center top / cover;
+  
+}
+.our-webcoderskull .cnt-block{ 
+   flex:right;
+   width:100%; 
+   background:#fff; 
+   padding:30px 20px; 
+   text-align:center; 
+   border:2px solid #d5d5d5;
+   margin: 0 0 28px;
+}
+.our-webcoderskull .cnt-block figure{
+
+   width:148px; 
+   height:148px; 
+   border-radius:100%; 
+   display:inline-block;
+   margin-bottom: 15px;
+}
+.our-webcoderskull .cnt-block img{ 
+   width:148px; 
+   height:148px; 
+   border-radius:100%; 
+}
+.our-webcoderskull .cnt-block h3{ 
+   color:#2a2a2a; 
+   font-size:20px; 
+   font-weight:500; 
+   padding:6px 0;
+   text-transform:uppercase;
+}
+.our-webcoderskull .cnt-block h3 a{
+  text-decoration:none;
+	color:#2a2a2a;
+}
+.our-webcoderskull .cnt-block h3 a:hover{
+	color:#337ab7;
+}
+.our-webcoderskull .cnt-block p{ 
+   color:#2a2a2a; 
+   font-size:13px; 
+   line-height:20px; 
+   font-weight:400;
+}
+.our-webcoderskull .cnt-block .follow-us{
+	margin:20px 0 0;
+}
+.our-webcoderskull .cnt-block .follow-us li{ 
+    display:inline-block; 
+	width:auto; 
+	margin:0 5px;
+}
+.our-webcoderskull .cnt-block .follow-us li .fa{ 
+   font-size:24px; 
+   color:#767676;
+}
+.our-webcoderskull .cnt-block .follow-us li .fa:hover{ 
+   color:#025a8e;
+}
+
+</style>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<section class="our-webcoderskull padding-lg">
+  <div class="container">
+    <div class="row heading heading-icon">
+        <h2>Our Team</h2>
     </div>
-    <div class="col mb-4">
-        <div class="card">
-            <img src="<?php echo base_url() . 'assets/images/bagas.jpg' ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Bagas Millen Alhafidz</h5>
-                <p class="card-text">bagas(1301180184) as a BackEnd</p>
-            </div>
-        </div>
-    </div>
-    <div class="col mb-4">
-        <div class="card">
-            <img src="<?php echo base_url() . 'assets/images/rizka.jpg' ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Rizka Nur Octaviani</h5>
-                <p class="card-text">Rizka(1301184125) as a FrontEnd.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col mb-4">
-        <div class="card">
-            <img src="<?php echo base_url() . 'assets/images/arya.jpg' ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Arya Pratama Anugerah</h5>
-                <p class="card-text">Arya(1301184336) as a FrontEnd.</p>
-            </div>
-        </div>
-    </div>
-</div>
+    <ul class="row">
+      <li class="col-6 col-md-6">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="<?php echo base_url(); ?>assets/images/rizka.jpg"></figure>
+            <h3>Rizka Nur Octaviani</h3>
+            <p>Front-End</p>
+            <h6>1301184125</h6>
+          </div>
+      </li>
+      <li class="col-6 col-md-6">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="<?php echo base_url(); ?>assets/images/drea.jpg" class="img-responsive" alt=""></figure>
+            <h3>Andrea Rahmadanisya</h3>
+            <p>Back-end</p>
+            <h6>1301174035</h6>
+          </div>
+      </li>
+      <div>
+      
+      <ul class="row">
+      <li class="col-6 col-md-4">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+          <figure><img src="<?php echo base_url(); ?>assets/images/arya.jpg"></figure>
+            <h3>Arya pratama anugerah R</h3>
+            <p>Front-End</p>
+            <h6>1301184336</h6>
+          </div>
+       </li>
+      <li class="col-6 col-md-4">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+          <figure><img src="<?php echo base_url(); ?>assets/images/bagas.jpg" class="img-responsive" alt=""></figure>
+            <h3>Bagaz milen alhafidz</h3>
+            <p>Back-end</p</p>
+            <h6>1301180184</h6>
+            <br>
+          </div>
+      </li>
+      <li class="col-6 col-md-4">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+          <figure><img src="<?php echo base_url(); ?>assets/images/teteh.jpg" float ="right"></figure>
+            <h3>Atika Suci Handayan</h3>
+            <p>Front-End</p>
+            <h6>1301174035</h6>
+          </div>
+       </li>
+    </ul>
+    </ul>
+  </div>
+</section>
