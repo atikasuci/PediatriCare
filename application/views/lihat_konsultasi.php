1bbad74c303c6e9f453dad92051eb2cdde1a75f6
fenrilius <52090483+fenrilius@users.noreply.github.com>
2020-04-19 22:14:53 +0700

    Add files via upload

---
+++ application/views/lihat_konsultasi.php
@@ -0,0 +1,47 @@
+<style>
+    footer{
+    background-color:#007B9B;
+}
+    .btn{
+        size:50px;
+        color:#fff;
+        background-color:#007B9B;
+    }
+    .card{
+        padding-left:160px;
+        padding-right:160px;
+        border:0px;
+    }
+    .card-jawab{
+        padding-left:30px;
+    }
+    </style>
+
+<br><br><br>
+<div class="card">
+  <div class="card-header">
+    <h4><b>Saya pernah duduk bersebelahan dengan ODP covid19, apakah saya harus isolasi diri?</b></h4>
+    <p>penanya : SAEPUDIN</p>
+  </div>
+  <div class="card-body">
+    <p class="card-text">sabtu siang kemarinsaya sempat bincang-bincang dengan teman-teman sekantor di ruangan tamu, 
+    apa yang harus saya lakukan untuk upaya penanggulangan covid dengan kondisi resiko yang saya alami ini dok? untuk informasi tambahan,
+     teman saya itu tidak ada gejala apapun, batuk pilek atau sakit tenggorokan, kami juga tidak berjabat tangan hanya berbincang seperti biasa, 
+     jarak duduk kami pun sekitar 60 meter.terimakasih dok atas perhatiannya...wassalamualaikum wr wb.</p>
+  </div>
+  <div class="card-footer">
+    <p>Dijawab oleh : tatang sutisna</p>
+    <p class="card-jawab">sabtu siang kemarinsaya sempat bincang-bincang dengan teman-teman sekantor di ruangan tamu, 
+    karena tidak ada satu pun diantara kami yang berstatus ODP sebelumnya, maka kami ngobrol seperti biasa saja. 
+    baru kemudian pada hari minggu siSelamat siang AS,Demam adalah perubahan suhu tubuh yang lebih tinggi dari 37,5 derajat celsius yang bisa menjadi pertanda tubuh sedang melakukan perlawanan terhadap adanya infeksi pada tubuh. Untuk itu jika merasa demam ada baiknya memeriksa suhu tubuh menggunakan termometer.
+    Pusing sendiri juga berbeda dengan sakit kepala dimana pusing dirasakan seperti sensasi berputar, rasa tidak seimbang ataupun melayang. Sedangkan sakit kepala sendiri karakternya bisa seperti terikat, tertindih, seperti tertusuk atau lainnya. Jika mengalami demam dan pusing setiap sore dan sudah berlangsung beberapa hari maka ada baiknya memeriksakan diri agar dipastikan penyebabnya. Penyebab demam sendiri bisa beragam mulai dari demam tifoid, demam berdarah, infeksi saluran pernapasan, infeksi saluran pencernaan, infeksi saluran kemih dan lainnya. Untuk itu perlu diperhatikan kondisi dan gejala lain yang timbul untuk membantu dokter memastikan penyebabnya.
+    Untuk meredakan keluhan dapat dilakukan beberapa cara seperti perbanyak istirahat, perbanyak minum air putih, kompres kepala ketika demam timbul dengan air biasa atau air hangat, konsumsi obat pereda demam dan sakit kepala seperti parasetamol dan jaga asupan makanan agar tidak melewatkan waktu makan dan makan teratur karena pola hidup juga bisa memicu keluhan yang sama dengan apa yang dirasakan. Semoga bermanfaat.
+    Salam sehat,</p>
+  </div>
+  <br>
+  <br>
+  <left><a type="button" class="btn" href="<?= base_url('KonsultasiController'); ?>">back</a></left>
+</div>
+<br>
+  <br>
+<footer>.</footer>
