a7acda93c0fa7e7252c11367372f1e3ae054084d
rizkan03 <61906974+rizkan03@users.noreply.github.com>
2020-04-19 11:46:42 +0700

    Update Home.php

--- application/views/Home.php
+++ application/views/Home.php
@@ -1,6 +1,98 @@
-//ae
-<br><br>
-<h1> THIS IS HOME</h1>
-<p>WELCOME, <?= $user['username']; ?></p>
+<html lang="en">   
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/login.css">  -->
+    <!-- <style>.carousel-inner > .item > img,.carousel-inner > .item > a > img { width: 80%; margin: auto;}</style> -->
+</head>
+<body>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <h1 class="display-4">COVID-19 (NOVEL CORONAVIRUS)</h1>
+            <p class="lead">Pengunjung ke rumah sakit kami dibatasi hanya untuk satu anggota keluarga dan semua pengungjung akan diperiksa</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Read more</a>
+        </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="thumbnail">
+            <img src="assets/images/logo/1.png" width="450" height="300" class="img-responsive">
+          </div>
+        </div>
+        <div class="col">
+          <h1>Apa itu Pediatricare?</h1>
+          <p>Kidcare Children Therapy Center merupakan wadah untuk anak-anak dengan kebutuhan khusus dengan memberikan fasilitas yang lengkap dan penanganan yang tepat Kidcare diharapkan dapat membantu mengoptimalkan kemampuan yang dimiliki oleh anak-anak dengan kebutuhan khusus dan diharapkan mereka mampu untuk mandiri. <br>
+          Kidcare didukung oleh segenap tenaga terapis yang berkompeten lulusan Fisioterapis dan sarjana Psikologi. Selain itu dalam upaya tetap meningkatkan kualitas terapi KIDCARE juga berkerjasama dengan konsultan terapy dan juga Psikologi.</p>
+        </div>
+      </div>
+    </div>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ul class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ul>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="assets/images/home/slider_1.jpeg" class="d-block w-100" alt="..." width="500" height="300">
+        </div>
+        <div class="carousel-item">
+          <img src="assets/images/home/slider_1.jpeg" class="d-block w-100" alt="..." width="500" height="300">
+        </div>
+        <div class="carousel-item">
+          <img src="assets/images/home/slider_1.jpeg" class="d-block w-100" alt="..." width="500" height="300">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">
+          <h1>Tips Seputar Kesehatan</h1>
+        </div>
+      </div>
+    
+
+      <div class="row">
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body text-center" >
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="card-link">Card link</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body text-center" >
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="card-link">Card link</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+</body>
+</html>
 
 
