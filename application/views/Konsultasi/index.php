d0a6d2877710eb570406a3f8395db9433c1361e5
bagasmillena <62025719+bagasmillena@users.noreply.github.com>
2020-04-20 16:21:11 +0700

    update all

--- application/views/Konsultasi/index.php
+++ application/views/Konsultasi/index.php
@@ -3,29 +3,26 @@
 
 <br><br><br><br>
 <?php foreach ($konsultasi as $k) { ?>
-<div class="card text-left">
-    <div class="card-header">
-        <b><?= $k['judul'] ?></b> <!-- judul -->
+    <div class="card text-left">
+        <div class="card-header">
+            <b><?= $k['judul'] ?></b> <!-- judul -->
+        </div>
+        <div class="card-body">
+            <h7 class="card-title"><b>Penanya : <?= $k['username'] ?></b></h7>
+            <br>
+            <!-- ini isi pertanyaan -->
+            <p class="card-text"><?= $k['question'] ?></p>
+        </div>
+        <div class="card-footer">
+            <a type="button" href="<?= base_url('lihat_konsultasi_controller'); ?>" class="btn" > lihat</a>
+        </div>
     </div>
-    <div class="card-body">
-        <h7 class="card-title"><b>Penanya : <?= $k['username'] ?></b></h7>
-        <br>
-        <!-- ini isi pertanyaan -->
-        <p class="card-text"><?= $k['question'] ?></p>
-    </div>
-    <div class="card-footer">
-        <a type="button" href="<?= base_url('lihat_konsultasi_controller'); ?> class="btn" style="color: white;"> lihat</a>
-        <a type="button" href="<?= base_url('InputKonsultasiController'); ?>">Sign Up</a>
-    </div>
-</div>
 <?php } ?>
+<br>
+<div class="card text-left">
+    <a type="button" href="<?= base_url('InputKonsultasiController'); ?>">Tanya!</a>
+</div>
 
 </table>
 <br></bbr>
 </div>
-
-
-<br>
-<br>
-<br>
-<footer>.</footer>
