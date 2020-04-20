389543d09aef02658398ecaa9c3e89e44be5004d
bagasmillena <62025719+bagasmillena@users.noreply.github.com>
2020-04-20 15:35:50 +0700

    hapus header

--- application/views/Konsultasi/index.php
+++ application/views/Konsultasi/index.php
@@ -1,12 +1,3 @@
-<html>
-
-<head>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="<?php base_url(); ?>assets/css/konsultasi.css" type="text/css" />
-
-</head>
 
 <img class="card-img" src="<?php echo base_url(); ?>assets/images/konsul/bg.png">
 
