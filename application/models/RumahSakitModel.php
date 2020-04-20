30cdb565b2ccde8ed55ac562e38df8c190b76324
bagasmillena <62025719+bagasmillena@users.noreply.github.com>
2020-04-20 18:09:50 +0700

    replace all

--- application/models/RumahSakitModel.php
+++ application/models/RumahSakitModel.php
@@ -17,18 +17,11 @@ class RumahSakitModel extends CI_Model
     public function deleteRumahSakit($id)
     {
         return $this->db->delete('rumahsakit', ['id_rumahsakit' => $id]);
-        // $this->db->where('id', $id);
-        // return $this->db->delete('rumahsakit');
     }
 
-    public function updateRumahSakit($id)
+    public function updateRumahSakit($id,$data)
     {
-        $data = [
-            "id_rumahsakit" => $id,
-            "nama" => $this->input->post('nama', true),
-            "alamat" => $this->input->post('alamat', true),
-            "notelp" => $this->input->post('notelp', true)
-        ];
+        
         $this->db->where('id_rumahsakit', $id);
         $this->db->update('rumahsakit', $data);
     }
