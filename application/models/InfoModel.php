61ca23456e9f3cd1a62ef58268ea950a0bda86f7
bagasmillena <62025719+bagasmillena@users.noreply.github.com>
2020-04-20 19:55:24 +0700

    dibikin simple

--- application/models/InfoModel.php
+++ application/models/InfoModel.php
@@ -23,36 +23,11 @@ class InfoModel extends CI_Model
 
     public function AddInfo($data)
     {
-        $config['upload_path']         = 'assets/info';  // folder upload 
-        $config['allowed_types']        = 'gif|jpg|png'; // jenis file
-        $config['max_size']             = 1000;
-        $config['max_width']            = 500;
-        $config['max_height']           = 500;
-
-        $this->load->library('upload', $config);
-
-        if (!$this->upload->do_upload('photo')) //sesuai dengan name pada form 
-        {
-            echo 'anda gagal upload';
-        }
-        $file = $this->upload->data();
-        $gambar = $file['file_name'];
-
-        $data = [
-            "judul" => $this->input->post('judul', true),
-            "isi" => $this->input->post('isi', true),
-            "photo" => $gambar
-        ];
         return $this->db->insert('info', $data);
     }
 
-    public function UpdateInfo($id_info)
+    public function UpdateInfo($id_info,$data)
     {
-        $data = [
-            "judul" => $this->input->post('judul', true),
-            "isi" => $this->input->post('isi', true),
-        ];
-
         $this->db->where('id_info', $id_info);
         $this->db->update('info', $data);
     }
@@ -60,35 +35,5 @@ class InfoModel extends CI_Model
     public function DeleteInfo($id_info)
     {
         return $this->db->delete('info', ['id_info' => $id_info]);
-    }
-
-    public function upload()
-    {
-        $config['upload_path'] = './upload/info/';
-        $config['allowed_types'] = 'jpg|png|jpeg';
-        $config['max_size']    = '2048';
-        $config['remove_space'] = TRUE;
-
-        $this->load->library('upload', $config);
-        if ($this->upload->do_upload('input_gambar')) {
-            // Kalo bisa nih bree :
-            $return = array('result' => 'success', 'file' => $this->upload->data(), 'error' => '');
-            return $return;
-        } else {
-            // Waduh gagal euy kesini mah :
-            $return = array('result' => 'failed', 'file' => '', 'error' => $this->upload->display_errors());
-            return $return;
-        }
-    }
-
-    public function save($upload)
-    {
-        $data = array(
-            'judul' => $this->input->post('input_judul'),
-            'isi' => $this->input->post('input_isi'),
-            'photo' => $upload['file']['file_name']
-        );
-
-        $this->db->insert('info', $data);
     }
 }
