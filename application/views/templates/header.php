84d03c05d9d6afb5c5aa9afac00d0f221ee578e6
bagasmillena <62025719+bagasmillena@users.noreply.github.com>
2020-04-20 15:32:19 +0700

    update header(link to css)

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,10 +8,13 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     <!-- MY CSS -->
 
-    <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/styles.css">
 
     <title><?php echo $judul ?></title>
 </head>
