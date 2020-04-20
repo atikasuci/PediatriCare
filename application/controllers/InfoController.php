9885d3d711289a5b6282afdd0c684b019f65dc4e
bagasmillena <62025719+bagasmillena@users.noreply.github.com>
2020-04-20 19:56:08 +0700

    insyaallah bisa

--- application/controllers/InfoController.php
+++ application/controllers/InfoController.php
@@ -15,7 +15,7 @@ class InfoController extends CI_Controller
         $data['judul'] = 'Info';
         $data['info'] = $this->InfoModel->getAllInfo();
         $this->load->view('templates/header', $data);
-        $this->load->view('Info/Index', $data);
+        $this->load->view('Info/index', $data);
         $this->load->view('templates/footer');
     }
 
@@ -45,18 +45,34 @@ class InfoController extends CI_Controller
         $data['judul'] = 'Form Ubah Info';
         $data['info'] = $this->InfoModel->getAllInfo();
 
-        //from library form_validation, set rules for nama, nim, email = required
         $this->form_validation->set_rules('judul', 'judul', 'required');
         $this->form_validation->set_rules('isi', 'isi', 'required');
 
+        $config['upload_path']          = './assets/images/info';  // folder upload 
+        $config['allowed_types']        = 'gif|jpg|png'; // jenis file
+        $config['max_size']             = 0;
+        $config['max_width']            = 0;
+        $config['max_height']           = 0;
+
+        $this->load->library('upload', $config);
+        $this->upload->initialize($config);
+        $this->upload->do_upload('gambar');
+        
+        $file = $this->upload->data();
+        $gambar = $file['file_name'];
+
+        $update = [
+            "judul" => $this->input->post('judul', true),
+            "isi" => $this->input->post('isi', true),
+            "photo" => $gambar
+        ];
         if ($this->form_validation->run() != false) {
-            $this->InfoModel->UpdateInfo($id_info);
-            $this->session->set_flashdata('flash', 'Diubah');
+            $this->InfoModel->UpdateInfo($id_info,$update);
             redirect('InfoController');
         } else {
-            //$this->load->view('templates/header', $data);
-            $this->load->view('Info/Indexcoba', $data);
-            // $this->load->view('templates/footer');
+            $this->load->view('templates/header', $data);
+            $this->load->view('Info/index', $data);
+            $this->load->view('templates/footer');
         }
     }
 
@@ -68,21 +84,38 @@ class InfoController extends CI_Controller
 
     public function Save()
     {
-        $data = array();
+        $data['judul'] = 'Form Add Info';
+        $data['info'] = $this->InfoModel->getAllInfo();
 
-        if ($this->input->post('submit')) {
-            $upload = $this->InfoModel->upload();
+        $this->form_validation->set_rules('judul', 'judul', 'required');
+        $this->form_validation->set_rules('isi', 'isi', 'required');
 
-            if ($upload['result'] == "success") {
-                $this->InfoModel->save($upload);
+        $config['upload_path']          = './assets/images/info';  // folder upload 
+        $config['allowed_types']        = 'gif|jpg|png'; // jenis file
+        $config['max_size']             = 0;
+        $config['max_width']            = 0;
+        $config['max_height']           = 0;
 
-                redirect('InfoController');
-            } else {
-                $data['message'] = $upload['error'];
-            }
-        }
+        $this->load->library('upload', $config);
+        $this->upload->initialize($config);
+        $this->upload->do_upload('gambar');
+        
+        $file = $this->upload->data();
+        $gambar = $file['file_name'];
+
+        $add = [
+            "judul" => $this->input->post('judul', true),
+            "isi" => $this->input->post('isi', true),
+            "photo" => $gambar
+        ];
+        if ($this->form_validation->run() != false) {
+            $this->InfoModel->AddInfo($add);
             redirect('InfoController');
-//         $this->load->view('InfoController', $data);
+        } else {
+            $this->load->view('templates/header', $data);
+            $this->load->view('Info/index', $data);
+            $this->load->view('templates/footer');
+        }
     }
 
     public function view($id)
